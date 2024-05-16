1f9fc45a28455e3aa10d40e39a3ca6f5c73490b8
Todi <ravi.todi@dell.com>
2024-05-15 22:14:31 -0400

    homework Ravit week1

---
+++ submission/query_2.sql
@@ -0,0 +1,217 @@
+ --Query  populates the actors table one year at a time starting from 2018 to 2021
+ INSERT INTO
+  RaviT.actors
+ WITH
+  last_year AS (
+    SELECT
+      *
+    FROM
+      RaviT.actors
+    WHERE
+      current_year = 2017
+  ),
+  this_year AS (
+    SELECT
+      actor,
+	  actor_id,
+	  ARRAY_AGG(
+      ROW(
+        film,
+        votes,
+        rating,
+        film_id)) 
+  AS films,
+  avg(rating) avg_rating,
+  year  	  
+    FROM
+      bootcamp.actor_films 
+	       where year = 2018
+	group by actor,actor_id, year
+  )
+SELECT
+  COALESCE(ly.actor, ty.actor) AS actor,
+  COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+  CASE
+    WHEN ty.year IS NULL THEN ly.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NULL THEN  ty.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NOT NULL THEN ty.films|| ly.films
+  END AS films,
+  case when ty.avg_rating is null  then ly.quality_class
+  else
+	case when ty.avg_rating>8 then 'star' 
+		 when ty.avg_rating>7 and  ty.avg_rating<=8 then 'good' 
+		 when ty.avg_rating>6 and  ty.avg_rating<=7 then 'average' 
+		 when ty.avg_rating<=6 then 'bad' 
+    end
+  end as quality_class,
+  ty.year IS NOT NULL AS is_active,
+  COALESCE(ty.year, ly.current_year + 1) AS current_year
+FROM
+  last_year ly
+  FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id;
+  
+INSERT INTO
+  RaviT.actors
+ WITH
+  last_year AS (
+    SELECT
+      *
+    FROM
+      RaviT.actors
+    WHERE
+      current_year = 2018
+  ),
+  this_year AS (
+    SELECT
+      actor,
+	  actor_id,
+	  ARRAY_AGG(
+      ROW(
+        film,
+        votes,
+        rating,
+        film_id)) 
+  AS films,
+  avg(rating) avg_rating,
+  year  	  
+    FROM
+      bootcamp.actor_films 
+	       where year = 2019
+	group by actor,actor_id, year
+  )
+SELECT
+  COALESCE(ly.actor, ty.actor) AS actor,
+  COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+  CASE
+    WHEN ty.year IS NULL THEN ly.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NULL THEN  ty.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NOT NULL THEN ty.films|| ly.films
+  END AS films,
+  case when ty.avg_rating is null  then ly.quality_class
+  else
+	case when ty.avg_rating>8 then 'star' 
+		 when ty.avg_rating>7 and  ty.avg_rating<=8 then 'good' 
+		 when ty.avg_rating>6 and  ty.avg_rating<=7 then 'average' 
+		 when ty.avg_rating<=6 then 'bad' 
+    end
+  end as quality_class,
+  ty.year IS NOT NULL AS is_active,
+  COALESCE(ty.year, ly.current_year + 1) AS current_year
+FROM
+  last_year ly
+  FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id;
+
+INSERT INTO
+  RaviT.actors
+ WITH
+  last_year AS (
+    SELECT
+      *
+    FROM
+      RaviT.actors
+    WHERE
+      current_year = 2019
+  ),
+  this_year AS (
+    SELECT
+      actor,
+	  actor_id,
+	  ARRAY_AGG(
+      ROW(
+        film,
+        votes,
+        rating,
+        film_id)) 
+  AS films,
+  avg(rating) avg_rating,
+  year  	  
+    FROM
+      bootcamp.actor_films 
+	       where year = 2020
+	group by actor,actor_id, year
+  )
+SELECT
+  COALESCE(ly.actor, ty.actor) AS actor,
+  COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+  CASE
+    WHEN ty.year IS NULL THEN ly.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NULL THEN  ty.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NOT NULL THEN ty.films|| ly.films
+  END AS films,
+  case when ty.avg_rating is null  then ly.quality_class
+  else
+	case when ty.avg_rating>8 then 'star' 
+		 when ty.avg_rating>7 and  ty.avg_rating<=8 then 'good' 
+		 when ty.avg_rating>6 and  ty.avg_rating<=7 then 'average' 
+		 when ty.avg_rating<=6 then 'bad' 
+    end
+  end as quality_class,
+  ty.year IS NOT NULL AS is_active,
+  COALESCE(ty.year, ly.current_year + 1) AS current_year
+FROM
+  last_year ly
+  FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id;
+
+INSERT INTO
+  RaviT.actors
+ WITH
+  last_year AS (
+    SELECT
+      *
+    FROM
+      RaviT.actors
+    WHERE
+      current_year = 2020
+  ),
+  this_year AS (
+    SELECT
+      actor,
+	  actor_id,
+	  ARRAY_AGG(
+      ROW(
+        film,
+        votes,
+        rating,
+        film_id)) 
+  AS films,
+  avg(rating) avg_rating,
+  year  	  
+    FROM
+      bootcamp.actor_films 
+	       where year = 2021
+	group by actor,actor_id, year
+  )
+SELECT
+  COALESCE(ly.actor, ty.actor) AS actor,
+  COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+  CASE
+    WHEN ty.year IS NULL THEN ly.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NULL THEN  ty.films
+    WHEN ty.year IS NOT NULL
+    AND ly.films IS NOT NULL THEN ty.films|| ly.films
+  END AS films,
+  case when ty.avg_rating is null  then ly.quality_class
+  else
+	case when ty.avg_rating>8 then 'star' 
+		 when ty.avg_rating>7 and  ty.avg_rating<=8 then 'good' 
+		 when ty.avg_rating>6 and  ty.avg_rating<=7 then 'average' 
+		 when ty.avg_rating<=6 then 'bad' 
+    end
+  end as quality_class,
+  ty.year IS NOT NULL AS is_active,
+  COALESCE(ty.year, ly.current_year + 1) AS current_year
+FROM
+  last_year ly
+  FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id;  
+  
+ 
+  
+  
+  
